8eaf9994b7b7edf859893d99e223e4b20a59dfec
Luis Ramos <albano16@gmail.com>
2014-10-15 01:03:50 +0100

    Now correctly tells the time in PT
    Added third line
    Switched to system fonts

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include <pebble.h>
   
-#define BUFFER_SIZE 10
+#define MIN_BUFFER_SIZE 9
+#define HOUR_BUFFER_SIZE 6
   
-static GFont font,fontBold;
+#define BUFFER_SIZE 44
+  
+static GFont font;
+static GFont fontBold;
   
 static Window *s_main_window;
 static TextLayer *s_line1;
@@ -10,7 +14,7 @@ static TextLayer *s_line2;
 static TextLayer *s_line3;
 
 static const char* HOURS[] = {
-  "zero",
+  "doze",
   "uma",
   "duas",
   "três",
@@ -23,17 +27,21 @@ static const char* HOURS[] = {
   "dez",
   "onze",
   "doze",
-  "treze",
-  "catorze",
-  "quinze",
-  "dezasseis",
-  "dezassete",
-  "dezoito",
-  "dezanove"
+  "uma",
+  "duas",
+  "três",
+  "quatro",
+  "cinco",
+  "seis",
+  "sete",
+  "oito",
+  "nove",
+  "dez",
+  "onze"
 };
 
 static const char* MIN[] = {
-  "em ponto",
+  "hora",
   "um",
   "dois",
   "três",
@@ -55,8 +63,8 @@ static const char* MIN[] = {
   "dezanove"
 };
 
-
-static const char* TENS_NUMBERS[] = {
+static const char* TENS_MIN[] = {
+  "zero",
   "dez",
   "vinte",
   "trinta",
@@ -69,11 +77,15 @@ static char row_1_buffer[BUFFER_SIZE];
 static char row_2_buffer[BUFFER_SIZE];
 static char row_3_buffer[BUFFER_SIZE];
 
+static int lastTenMinute;
+static int lastHour;
+
 static void initialize_text_layer(TextLayer *text_layer, GFont fontToUse){
-  text_layer_set_background_color(text_layer, GColorBlack);
+  text_layer_set_background_color(text_layer, GColorClear);
   text_layer_set_text_color(text_layer, GColorWhite);
   text_layer_set_font(text_layer, fontToUse);
   text_layer_set_text_alignment(text_layer, GTextAlignmentLeft);
+  text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
   
   layer_add_child(window_get_root_layer(s_main_window), text_layer_get_layer(text_layer));
 }
@@ -81,23 +93,59 @@ static void initialize_text_layer(TextLayer *text_layer, GFont fontToUse){
 static void update_time() {
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
+  int minute = tick_time->tm_min;
+  int hour = tick_time->tm_hour;
+  //int minute = 17;
+  //int hour = 4;
   
-  memset(row_1_buffer, 0, BUFFER_SIZE);
-  memcpy(row_1_buffer, HOURS[tick_time->tm_hour], BUFFER_SIZE);
   
-  memset(row_2_buffer, 0, BUFFER_SIZE);
-  memcpy(row_2_buffer, MIN[tick_time->tm_min], BUFFER_SIZE);
+  if(minute < 20){
+    memset(row_2_buffer, 0, BUFFER_SIZE);
+    strcat(row_2_buffer, MIN[minute]);
+    text_layer_set_text(s_line2, row_2_buffer);
+    
+    if(!layer_get_hidden((Layer *)s_line3)){
+      layer_set_hidden((Layer *)s_line3, true); 
+    }
+  }else{
+    if(minute % 10 != 0){
+      if(layer_get_hidden((Layer *)s_line3)){
+        layer_set_hidden((Layer *)s_line3, false); 
+      }    
+      
+      memset(row_3_buffer, 0, BUFFER_SIZE);
+      strcat(row_3_buffer, MIN[minute % 10]);
+      text_layer_set_text(s_line3, row_3_buffer);
+    }else{
+      if(!layer_get_hidden((Layer *)s_line3)){
+        layer_set_hidden((Layer *)s_line3, true); 
+      }
+    }
+    
+    if(lastTenMinute != ((int) minute / 10)){
+      memset(row_2_buffer, 0, BUFFER_SIZE);
+      strcat(row_2_buffer, TENS_MIN[(int) minute/10]);
+      text_layer_set_text(s_line2, row_2_buffer);  
+    }
+  }
+  lastTenMinute = (int) minute / 10;
   
-  text_layer_set_text(s_line1, row_1_buffer);
-  text_layer_set_text(s_line2, row_2_buffer);
+  if(lastHour == -1 || lastHour != hour){
+    memset(row_1_buffer, 0, BUFFER_SIZE);
+    strcat(row_1_buffer, HOURS[hour]);
+    text_layer_set_text(s_line1, row_1_buffer);  
+  }
+  lastHour = hour;
+  
 }
 
 static void main_window_load(Window *window){
-  
-  s_line1 = text_layer_create(GRect(0, 50, 144, 50));
+  s_line1 = text_layer_create(GRect(0, 22, 144, 50));
   initialize_text_layer(s_line1, fontBold);
-  s_line2 = text_layer_create(GRect(0, 70, 144, 50));
+  s_line2 = text_layer_create(GRect(0, 59, 144, 50));
   initialize_text_layer(s_line2, font);
+  s_line3 = text_layer_create(GRect(0, 93, 144, 50));
+  initialize_text_layer(s_line3, font);
 }
 
 static void main_window_unload(Window *window){
@@ -120,8 +168,11 @@ static void init() {
     .unload = main_window_unload
   });
   
-  fontBold = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SANSATION_BOLD_30));
-  font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SANSATION_LIGHT_28));
+  fontBold = fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD);
+  font = fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT);
+  
+  lastHour = -1;
+  lastTenMinute = -1;
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
